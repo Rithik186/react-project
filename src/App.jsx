3cea6837dc0ee0db7efeabd16025d4f262c66553
Rithik <rithikkannaa@gmail.com>
2026-01-27 12:36:15 +0530

    react Router,Routes basic setup in App.jsx and main.jsx and creating diff files to check routing...

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,24 @@ import Profile from "./components/Profile"
 import State from "./hooks/State"
 import Parent from "./components/Parent"
 import Form from "./hooks/Form"
+import { Route,Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import Service from "./pages/Service"
 const App = () => {
   return (
     <>
-      <Profile name="Rithik" age={19} skills={["HTML","CSS","JS","PYTHON","MONGO-DB"]}/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/service" element={<Service/>}/>
+      </Routes>
+      {/* <Profile name="Rithik" age={19} skills={["HTML","CSS","JS","PYTHON","MONGO-DB"]}/>
       <State/>
       <Parent/>
-      <Form/>
+      <Form/> */}
     </>
 
   )

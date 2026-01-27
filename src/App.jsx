2e9d522db8280ee91cef3c92129178c0652ec400
Rithik <rithikkannaa@gmail.com>
2026-01-27 11:54:04 +0530

    created form and sending the values to console using prev, handlechanges, handlesubmit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Profile from "./components/Profile"
 import State from "./hooks/State"
 import Parent from "./components/Parent"
+import Form from "./hooks/Form"
 const App = () => {
   return (
     <>
       <Profile name="Rithik" age={19} skills={["HTML","CSS","JS","PYTHON","MONGO-DB"]}/>
       <State/>
       <Parent/>
+      <Form/>
     </>
 
   )

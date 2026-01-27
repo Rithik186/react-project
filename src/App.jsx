872217c7efbd0d13eedd64021a87d0801827c9fe
Rithik <rithikkannaa@gmail.com>
2026-01-27 12:57:37 +0530

    created a working Navbar Component and imported in App.jsx for : Home,About,Servies,Contact pages and updated the App.jsx with Routes tag and specify the Route path for each files... then moved the existing component calls such as Profiles,State... etc to Home.jsx and imported it...

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
-import Profile from "./components/Profile"
-import State from "./hooks/State"
-import Parent from "./components/Parent"
-import Form from "./hooks/Form"
 import { Route,Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Contact from "./pages/Contact"
 import Service from "./pages/Service"
+import Navbar from "./components/Navbar"
 const App = () => {
   return (
     <>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/contact" element={<Contact/>}/>
-        <Route path="/service" element={<Service/>}/>
+        <Route path="/services" element={<Service/>}/>
       </Routes>
-      {/* <Profile name="Rithik" age={19} skills={["HTML","CSS","JS","PYTHON","MONGO-DB"]}/>
-      <State/>
-      <Parent/>
-      <Form/> */}
+
     </>
 
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,16 @@
+import Profile from "../components/Profile"
+import State from "../hooks/State"
+import Parent from "../components/Parent"
+import Form from "../hooks/Form"
 const Home = () => {
   return (
+    <>
     <div>Home</div>
+    <Profile name="Rithik" age={19} skills={["HTML","CSS","JS","PYTHON","MONGO-DB"]}/>
+      <State/>
+      <Parent/>
+      <Form/>
+    </>
   )
 }
 

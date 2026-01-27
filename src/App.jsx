d30e1e7c78dd0acdb1d6a5d32cf0c5a4ef8953fd
Rithik <rithikkannaa@gmail.com>
2026-01-27 10:11:37 +0530

    state created , basic increment and decrement using onclick

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Profile from "./components/Profile"
+import State from "./hooks/State"
 const App = () => {
   return (
     <>
-      <Profile name="Rithik" age="19" skills={["HTML","CSS","JS","PYTHON","MONGO-DB"]}/>
-    
+      <Profile name="Rithik" age={19} skills={["HTML","CSS","JS","PYTHON","MONGO-DB"]}/>
+      <State/>
     </>
 
   )
--- src/hooks/State.jsx
+++ src/hooks/State.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import { useState } from "react"
 
 const State = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    const [count,setCount]=useState(0)
+    return(
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <button onClick={() => setCount(count - 1)}>Decrement</button>
+        </div>
+    )
 }
-
 export default State
